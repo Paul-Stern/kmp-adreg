5b081ac31da07d83dfcd3ffd740ce9925120cda2
Paul Stern <paulstern45@gmail.com>
2024-09-16 05:45:05 +0300

    Refactor: create separate login file.

---
+++ composeApp/src/commonMain/kotlin/org/example/project/Login.kt
@@ -0,0 +1,54 @@
+package org.example.project
+
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun LoginField() {
+    var login by remember { mutableStateOf("") }
+    TextField(
+        value = login,
+        onValueChange = {login = it},
+        label = { Text("Ваш логин") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Email
+        )
+    )
+}
+
+@Preview
+@Composable
+fun PasswordTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    val isPasswordVisible = remember { mutableStateOf(false) }
+    TextField(
+        value = value,
+        onValueChange = { onValueChange(it)},
+        label = { Text("Ваш пароль") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password,
+        ),
+        visualTransformation = when (isPasswordVisible.value) {
+            true -> VisualTransformation.None
+            false -> PasswordVisualTransformation()
+        }
+    )
+}
