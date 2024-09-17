721afcb7ac10a76793e5a1ce0177c2c2ba17eecd
Paul Stern <paulstern45@gmail.com>
2024-09-17 09:12:15 +0300

    refactor: Clean up
     1. Remove User file. Maybe add later
     2. Remove Login file. Not needed.
     3. Paste Login file code to LoginScreen

--- composeApp/src/commonMain/kotlin/business/main/User.kt
+++
@@ -1,6 +0,0 @@
-package business.main
-
-data class User(
-    val login: String = String(),
-    val password: String = String(),
-)
--- composeApp/src/commonMain/kotlin/org/example/project/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/LoginScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,7 +16,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import kotlinproject.composeapp.generated.resources.Res
+import kotlinproject.composeapp.generated.resources.login
+import kotlinproject.composeapp.generated.resources.password
 import org.example.project.ui.view_model.LoginState
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -50,4 +60,43 @@ fun LoginScreen(
 
         }
     }
+}
+@Preview
+@Composable
+fun LoginField(
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    TextField(
+        value = value,
+        onValueChange = { onValueChange(it)},
+        label = { Text(stringResource(resource = Res.string.login)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Next,
+            keyboardType = KeyboardType.Email
+        )
+    )
+}
+@Preview
+@Composable
+fun PasswordField(
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    val isPasswordVisible = remember { mutableStateOf(false) }
+    TextField(
+        value = value,
+        onValueChange = { onValueChange(it)},
+        label = { Text(stringResource(resource = Res.string.password)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password,
+        ),
+        visualTransformation = when (isPasswordVisible.value) {
+            true -> VisualTransformation.None
+            false -> PasswordVisualTransformation()
+        }
+    )
 }
