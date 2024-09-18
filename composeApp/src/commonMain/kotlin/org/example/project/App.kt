44c77e12a9ecdc5f39e2743afdf26b78d5bc08ae
Paul Stern <paulstern45@gmail.com>
2024-09-18 11:13:28 +0300

    splash:
      Create AppNavigation class with route list
      Start working on splash

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -3,15 +3,14 @@
 package org.example.project
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import org.example.project.ui.view_model.LoginState
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -19,5 +18,13 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 
 fun App() {
-    LoginScreen(LoginState()) // TODO: Implement navigateToMain():
+//    LoginScreen(LoginState()) // TODO: Implement navigateToMain():
+    // TODO: Complete splash screen
+    MaterialTheme {
+        val navigator = rememberNavController()
+
+        Box(modifier = Modifier.fillMaxSize()) {
+
+        }
+    }
 }
---
+++ composeApp/src/commonMain/kotlin/org/example/project/navigation/AppNavigation.kt
@@ -0,0 +1,15 @@
+package org.example.project.navigation
+
+import kotlinproject.composeapp.generated.resources.Res
+import kotlinproject.composeapp.generated.resources.app_name
+import kotlinproject.composeapp.generated.resources.login_screen
+import kotlinproject.composeapp.generated.resources.registry
+import org.jetbrains.compose.resources.StringResource
+
+enum class AppNavigation (val title: StringResource) {
+    //    Start(title = Res.string.app_name)
+    Splash(title = Res.string.app_name),
+    Login(title = Res.string.login_screen),
+    Registry(title = Res.string.registry)
+}
+
