f1ba1ace41d89a549972259e3a7ed5c0c0354a4d
Paul Stern <paulstern45@gmail.com>
2024-09-19 13:40:50 +0300

    home: create HomeViewModel. TODO: implement basics

---
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/main/home/view_model/HomeViewModel.kt
@@ -0,0 +1,10 @@
+package org.example.project.ui.main.home.view_model
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel() : ViewModel() {
+
+//    val state: MutableState<HomeState> = mutableStateOf(HomeState())
+}
