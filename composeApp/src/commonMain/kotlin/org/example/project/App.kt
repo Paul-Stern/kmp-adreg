f54424092ced696e477ce5704e090a93473e42ea
Paul Stern <paulstern45@gmail.com>
2024-09-19 07:12:27 +0300

    refactor: export code to SplashScrenn.kt

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -10,12 +10,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavGraph
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.app_name
 import kotlinproject.composeapp.generated.resources.company
 import kotlinproject.composeapp.generated.resources.compose_multiplatform
+import org.example.project.navigation.AppNavigation
 import org.example.project.ui.view_model.LoginState
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.imageResource
@@ -24,27 +26,45 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-
 fun App() {
 //    LoginScreen(LoginState()) // TODO: Implement navigateToMain():
     // TODO: Complete splash screen
+    // SEE: SpashScreen.kt file
     MaterialTheme {
         val navigator = rememberNavController()
 
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-//            healthcareSplash(
-//                resource = imageResource(Res.drawable)
-//            )
-            Column {
-                Text(text = stringResource(Res.string.company))
-                Text(text = stringResource(Res.string.app_name))
-            }
+        /*
+        Box(modifier = Modifier.fillMaxSize()) {
+            NavHost(
+                navController = navigator,
+                graph = AppNavigation.Splash
+
+//                graph =
+//                modifier = Modifier.fillMaxSize(),
+            )
         }
+
+         */
+
+        SplashScreen()
+        /*
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+        //            healthcareSplash(
+        //                resource = imageResource(Res.drawable)
+        //            )
+                Column {
+                    Text(text = stringResource(Res.string.company))
+                    Text(text = stringResource(Res.string.app_name))
+                }
+            }
+         */
     }
+
 }
+
 /*
 @Composable
 fun healthcareSplash(resource: DrawableResource): ImageVector {
---
+++ composeApp/src/commonMain/kotlin/org/example/project/SplashScreen.kt
@@ -0,0 +1,56 @@
+package org.example.project
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import kotlinproject.composeapp.generated.resources.Res
+import kotlinproject.composeapp.generated.resources.app_name
+import kotlinproject.composeapp.generated.resources.company
+import kotlinx.coroutines.delay
+import org.example.project.ui.view_model.LoginEvent
+import org.example.project.ui.view_model.LoginState
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+@Preview
+internal fun SplashScreen(
+//    state: LoginState,
+//    events: (LoginEvent) -> Unit,
+//    navigateToMain: () -> Unit,
+//    navigateToLogin: () -> Unit,
+) {
+//    LaunchedEffect(state.navigateToMain) {
+//        delay(3000L)
+//        if (state.navigateToMain) {
+//            navigateToMain()
+//        } else {
+//            navigateToLogin()
+//        }
+//    }
+    MaterialTheme {
+        val navigator = rememberNavController()
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+//            healthcareSplash(
+//                resource = imageResource(Res.drawable)
+//            )
+            Column {
+                Text(text = stringResource(Res.string.company))
+                Text(text = stringResource(Res.string.app_name))
+            }
+        }
+    }
+}
