2a420ee24a77a478b0be61de3ff1b857b5e72a4c
Paul Stern <paulstern45@gmail.com>
2024-09-19 09:40:52 +0300

    splash: Add NavHost. draft

--- composeApp/src/commonMain/kotlin/org/example/project/ui/SplashNav.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/SplashNav.kt
@@ -1,6 +1,9 @@
 package org.example.project.ui.splash
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 @Composable
@@ -8,4 +11,9 @@ internal fun SplashNav(
     navigateToMain: () -> Unit,
 ) {
     val navigator = rememberNavController()
+    NavHost(
+        navController = navigator,
+        modifier = Modifier.fillMaxSize(),
+        startDestination = TODO()
+    ) {}
 }
