a4b1bff66524ca775488c877b5027039e12a72c8
Paul Stern <paulstern45@gmail.com>
2024-09-19 10:33:33 +0300

    mainNav: impove MainNav
    TODO: Create MainNavigation

--- composeApp/src/commonMain/kotlin/org/example/project/ui/main/MainNav.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/main/MainNav.kt
@@ -1,6 +1,20 @@
 package org.example.project.ui.main
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun MainNav(logout: () -> Unit) {}
+fun MainNav(logout: () -> Unit) {
+    // see: https://github.com/razaghimahdi/Shopping-By-KMP/blob/fe055734130d24461c6340f104666a083f7413a1/shared/src/commonMain/kotlin/presentation/ui/main/MainNav.kt#L40:w
+
+    val navController = rememberNavController()
+
+    Box() {
+        NavHost(
+            navController = navController,
+            startDestination = TODO(),
+        ) {}
+    }
+}
