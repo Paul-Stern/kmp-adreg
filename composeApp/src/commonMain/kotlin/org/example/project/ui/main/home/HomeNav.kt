f33e5d9b7d7a890c2e600ba542a6abe1ec91bbe3
Paul Stern <paulstern45@gmail.com>
2024-09-19 10:53:27 +0300

    home: Create HomeNav. Draft
    TODO: implement basic functionality

---
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/main/home/HomeNav.kt
@@ -0,0 +1,23 @@
+package org.example.project.ui.main.home
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import org.example.project.navigation.HomeNavigation
+
+// see: https://github.com/razaghimahdi/Shopping-By-KMP/blob/fe055734130d24461c6340f104666a083f7413a1/shared/src/commonMain/kotlin/presentation/ui/main/home/HomeNav.kt#L21:w
+
+
+@Composable
+fun HomeNav(
+    logout: () -> Unit,
+) {
+    val navigator = rememberNavController()
+
+    NavHost(
+        startDestination = HomeNavigation.Home.route,
+        navController = navigator,
+    ) {
+
+    }
+}
