7bd43dd10ec325cb38295d7ce595a40d255cd641
Paul Stern <paulstern45@gmail.com>
2024-09-20 09:17:15 +0300

    koin: add to App.kt

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -13,9 +13,17 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.example.project.navigation.AppNavigation
 import org.example.project.ui.splash.SplashNav
+import org.koin.compose.KoinApplication
+import org.koin.dsl.koinApplication
+
+fun koinConfiguration() = koinApplication {
+    modules()
+}
 
 @Composable
 fun App() {
+    // see: https://insert-koin.io/docs/reference/koin-compose/compose/#koin-compose-multiplatform-vs-koin-android-jetpack-compose
+//    KoinApplication(::koinConfiguration) { // paired with LN64
 //    LoginScreen(LoginState()) // TODO: Implement navigateToMain():
     // TODO: Complete splash screen
     // SEE: SpashScreen.kt file
@@ -31,25 +39,26 @@ fun App() {
                 composable(route = AppNavigation.Splash.route) {
 //                    SplashNav // TODO:
                     /*
-                    SplashNav(
-                        navigateToMain = {
-                            navigator.popBackStack()
+                SplashNav(
+                    navigateToMain = {
+                        navigator.popBackStack()
 //                            navigator.navigate(AppNavigation.Login.route)
-                            navigator.navigate(AppNavigation.Main.route)
-                        }
-                    )
-                    `
-                     */
+                        navigator.navigate(AppNavigation.Main.route)
+                    }
+                )
+                `
+                 */
                 }
                 composable(route = AppNavigation.Main.route) {
                     /*
-                    MainNav { // TODO: implement it
-                        navigator.popBackStack()
-                        navigator.navigate(AppNavigation.Splash.route)
-                    }
-                     */
+                MainNav { // TODO: implement it
+                    navigator.popBackStack()
+                    navigator.navigate(AppNavigation.Splash.route)
+                }
+                 */
                 }
             }
         }
     }
 }
+//}
