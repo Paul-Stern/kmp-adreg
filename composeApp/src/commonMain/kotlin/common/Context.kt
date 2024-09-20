14abcacf6688fc6541fb8150587a0e5d1769ec66
Paul Stern <paulstern45@gmail.com>
2024-09-20 10:05:49 +0300

    koin: remove custom Context

--- composeApp/src/commonMain/kotlin/common/Context.kt
+++
@@ -1,9 +0,0 @@
-package common
-
-// see: https://github.com/razaghimahdi/Shopping-By-KMP/blob/master/shared/src/commonMain/kotlin/common/Context.kt
-//expect class Context
-//see: https://github.com/razaghimahdi/Shopping-By-KMP/blob/master/shared/src/androidMain/kotlin/common/Context.kt
-/*
-import android.app.Application
-
-actual typealias Context = Application
