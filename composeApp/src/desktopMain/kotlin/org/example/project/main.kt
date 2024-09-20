43f2b00e0380793bc4b16facc0b3f52ced0313c4
Paul Stern <paulstern45@gmail.com>
2024-09-20 16:17:18 +0300

    main: change main window title

--- composeApp/src/desktopMain/kotlin/org/example/project/main.kt
+++ composeApp/src/desktopMain/kotlin/org/example/project/main.kt
@@ -2,11 +2,14 @@ package org.example.project
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinproject.composeapp.generated.resources.Res
+import kotlinproject.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "KotlinProject",
+        title = stringResource(Res.string.app_name),
     ) {
         App() //
     }
