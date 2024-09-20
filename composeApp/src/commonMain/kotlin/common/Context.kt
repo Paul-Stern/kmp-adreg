14425ee258992ed659a14c0d48c78d3ba486b02f
Paul Stern <paulstern45@gmail.com>
2024-09-20 10:04:25 +0300

    koin: create Koin appModule in Koin.kt di package
    Create context file. To be removed use KoinContext instead.

---
+++ composeApp/src/commonMain/kotlin/common/Context.kt
@@ -0,0 +1,9 @@
+package common
+
+// see: https://github.com/razaghimahdi/Shopping-By-KMP/blob/master/shared/src/commonMain/kotlin/common/Context.kt
+//expect class Context
+//see: https://github.com/razaghimahdi/Shopping-By-KMP/blob/master/shared/src/androidMain/kotlin/common/Context.kt
+/*
+import android.app.Application
+
+actual typealias Context = Application
---
+++ composeApp/src/commonMain/kotlin/di/Koin.kt
@@ -0,0 +1,6 @@
+package di
+
+import org.koin.core.context.KoinContext
+import org.koin.dsl.module
+
+fun appModule(context: KoinContext) = module{}
