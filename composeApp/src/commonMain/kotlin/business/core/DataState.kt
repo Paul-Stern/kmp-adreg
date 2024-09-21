4161a7e5fb489361c375810c5b00518279efdf8e
Paul Stern <paulstern45@gmail.com>
2024-09-21 14:04:43 +0300

    business: implement DataState

--- composeApp/src/commonMain/kotlin/business/core/DataState.kt
+++ composeApp/src/commonMain/kotlin/business/core/DataState.kt
@@ -2,7 +2,7 @@ package business.core
 
 sealed class DataState<T> {
 
-//    data class Response<T>(val uiComponent: UIComponent) : DataState<T>() // TODO: implement UIComponent
+    data class Response<T>(val uiComponent: UIComponent) : DataState<T>()
 
     data class Data<T>(val data: T? = null, val status: Boolean? = null) : DataState<T>()
 
