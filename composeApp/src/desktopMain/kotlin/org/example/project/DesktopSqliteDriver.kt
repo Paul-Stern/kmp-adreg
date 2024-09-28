30f6baf7d9fd6b9f062562a3ad40e0419f2f2578
Paul Stern <paulstern45@gmail.com>
2024-09-28 16:52:25 +0300

    sql: desktop: create driver (draft?)

---
+++ composeApp/src/desktopMain/kotlin/org/example/project/DesktopSqliteDriver.kt
@@ -0,0 +1,18 @@
+package org.example.project.db.common
+
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import org.example.project.db.Users
+
+// see: https://cashapp.github.io/sqldelight/2.0.2/multiplatform_sqlite/#constructing-driver-instances
+
+//import org.example.Use
+
+class DesktopDriverFactory {
+    fun createDriver(): SqlDriver {
+        val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        Users.Schema.create(driver)
+        return driver
+    }
+}
+
