80f7f66b4d79789b74a11ac07aa591500963b2a8
Paul Stern <paulstern45@gmail.com>
2024-09-30 11:09:06 +0300

    sdk: start creating

---
+++ composeApp/src/commonMain/kotlin/RegistrySDK.kt
@@ -0,0 +1,20 @@
+package org.example.project.db
+
+import business.core.db.User
+import org.example.project.db.common.Database
+import org.example.project.db.common.DatabaseDriverFactory
+import kotlin.jvm.Throws
+
+class RegistrySDK (databaseDriverFactory: DatabaseDriverFactory) {
+    private val db = Database(databaseDriverFactory)
+
+    @Throws(Exception::class)
+    suspend fun getUsers(forceReload: Boolean):  List<User> {
+        val cachedUsers = db.getAllUsers()
+        return if (cachedUsers.isNotEmpty() && !forceReload) {
+            cachedUsers
+        } else {
+            db.getAllUsers() // TODO: logic seems broken see LN13
+        }
+    }
+}
