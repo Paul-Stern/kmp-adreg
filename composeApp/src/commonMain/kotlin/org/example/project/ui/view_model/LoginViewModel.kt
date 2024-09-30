faba96c0c0accc975756f1f3963916362f9aea83
Paul Stern <paulstern45@gmail.com>
2024-09-30 18:17:38 +0300

    login: start fixing event handling in viewModel

--- composeApp/src/commonMain/kotlin/org/example/project/ui/view_model/LoginViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/view_model/LoginViewModel.kt
@@ -13,6 +13,41 @@ class LoginViewModel(
     val state: MutableState<LoginState> = mutableStateOf(LoginState())
 
     // TODO: finish LoginViewModel
+
+    fun onTriggerEvent(event: LoginEvent) { // TODO: Call it somewhere
+        when (event) {
+
+            is LoginEvent.OnUpdateNameRegister -> {
+                TODO()
+            }
+
+            is LoginEvent.Login -> {
+                TODO()
+            }
+
+            is LoginEvent.OnUpdatePasswordLogin -> {
+                onUpdatePasswordLogin(event.value)
+            }
+            else -> {
+                TODO()
+            }
+
+        }
+    }
+
+
+    private fun onUpdatePasswordLogin(value: String) {
+        state.value = state.value.copy(passwordLogin = value)
+    }
+
+    /*
+    private fun onUpdateNameRegister(value: String) {
+        state.value = state.value.copy(nameRegister = value)
+    }
+
+     */
+
+
     /*
     private fun checkToken() {
         checkTokenInteractor.execute().onEach { dataState ->
